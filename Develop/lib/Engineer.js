f9f43d60688c1b794ecf29d66b85802f1452e8c6
Ashvini Thomas <ashvinithomas@gmail.com>
2020-11-27 15:58:16 -0500

    created Engineer class with properties and methods

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,2 +1,32 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-class Engineer extends Employee {}
+class Engineer extends Employee {
+
+    constructor(name, id, email, github) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.github = github;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId () {
+        return this.id;
+    }
+
+    getEmail () {
+        return this.email;
+    }
+
+    getGithub () {
+        return this.github;
+    }
+
+    getRole () {
+        return "Engineer";
+    }
+}
+
+module.exports = Engineer;
